d26c7be5efe422d73bbf0f7bc578ad1947befb80
rasmiranjanpatnaik <rasmiranjanpatnaik2@gmail.com>
2026-03-12 15:26:09 +0530

    changes

--- product-portfolio/src/pages/UberKidsCaseStudy.tsx
+++ product-portfolio/src/pages/UberKidsCaseStudy.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import ScrollReveal from '../components/ScrollReveal';
 import './UberKidsCaseStudy.css';
